3c49e1587cb8f2efad7004647e8cb0297924b77a
Peter Trueblood <Peter.Trueblood@orthofi.com>
2021-04-24 20:57:13 -0600

    feat: set target environment

---
+++ src/main.ts
@@ -0,0 +1,39 @@
+import * as core from '@actions/core'
+
+async function run(): Promise<void> {
+  try {
+    let target = ''
+
+    const environmentName = core.getInput('environment-name')
+    if (environmentName) {
+      target = environmentName
+    } else {
+      const branch = core
+        .getInput('branch', {required: true})
+        .replace('refs/heads/', '')
+
+      switch (branch) {
+        case 'main':
+        case 'master':
+          target = 'production'
+          break
+        case 'risk':
+        case 'regression':
+          target = 'risk'
+          break
+        default:
+          target = branch
+          break
+      }
+    }
+
+    if (target) {
+      core.info(`Setting target environment to ${target}`)
+      core.setOutput('target-environment', target)
+    }
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+run()
